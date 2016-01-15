21de14956913f845850058f5546d9927d651f0a5
Anthony Val  C <anthonysinbad@gmail.com>
2016-01-15 17:59:52 +0800

    1
    1

--- trial_matlab_codes/HaversineFormula.m
+++ trial_matlab_codes/HaversineFormula.m
@@ -1,21 +1,19 @@
 clear all;
-% Based on Haversine Formula - finding distances based on latitude and
-% longitude. For more information visit :http://andrew.hedges.name/experiments/haversine/
 
-
-% Author : Anthony Val C. Camposano
+% Finding distances based on latitude and longitude using the Haversine Formula 
+% For more information visit :http://andrew.hedges.name/experiments/haversine/
+% Author/s : Anthony Val C. Camposano, Jenny Lynn Almerol, Kahlil Fredirick Chui
 % Date    : 1/15/2016
-
-
 
 data = xlsread('D:\Complex System\Data\Bohol_Earthquake_500KM_rawData.xls','L2:M9802'); 
 %total number of points: 9802
 
-R = 6373;
+R = 6373; % Earth's Radius
 
 for i = 1:length(data)
     dMatrix(i+1,1) = i;
     dMatrix(1,i+1) = i;
+    
     for j = 1:length(data)
     % X = longitude, first column 
     % Y = latitude, second column
@@ -30,9 +28,11 @@ for i = 1:length(data)
     dlon = (lon2 - lon1);
     dlat = (lat2 - lat1);
     
+    % Haversine Formula
     a = (sin(dlat/2))^2 + cos(lat1) * cos(lat2) * (sin(dlon/2))^2;
     c = 2 * atan2(sqrt(a), sqrt(1-a)); 
     d = R * c;
+    
     nD = d/125; 
     if nD > 1
         dMatrix(i+1,j+1) = 0;
@@ -45,4 +45,4 @@ for i = 1:length(data)
 end 
 
 
-csvwrite('D:\Complex System\BoholEarthquake_distMatrix_500km.dat',dMatrix);
+% csvwrite('D:\Complex System\BoholEarthquake_distMatrix_500km.dat',dMatrix);
---
+++ trial_matlab_codes/ScaleFreeNetworksReplication.m
@@ -0,0 +1,4 @@
+data = csvread('J:\Physics\Complex System\Literature Replication\1984.catalog.txt');
+
+% [YYY,MM,DD,HH,mm,SS,LATITUDE,LONGITUDE,Q,MAG,DEPTH,NPH,RMS,EVID]
+
