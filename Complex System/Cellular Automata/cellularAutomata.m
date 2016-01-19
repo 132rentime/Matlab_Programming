54b3cfeac3214f26d446fb53adfe4ed1e574a298
Anthony Val  C <anthonysinbad@gmail.com>
2016-01-19 11:59:31 +0800

    Game of Life in matlab
    a little progress in the implementation of the rules

--- Complex System/Cellular Automata/cellularAutomata.m
+++ Complex System/Cellular Automata/cellularAutomata.m
@@ -4,4 +4,24 @@ clear all
 % Date  : 1/19/2015
 
 
+% Game of Life
+% 0 is black
+% 255 is white
 
+L = 10;
+size = round(0.30*L);
+x = [0,255];
+grid = zeros(L,L);
+world_grid = zeros(L+2,L+2);
+% initial state
+% starting size = 10
+for i=1:size
+    xloc = randi(L,1);
+    yloc = randi(L,1);
+    grid(xloc,yloc) = datasample(255,1);
+end
+
+world_grid(2:L+1,2:L+1) = grid(:,:);
+
+location = gameoflifeRule(grid,L,size);
+image((grid));
